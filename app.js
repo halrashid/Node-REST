ebd56151407b4ce0fc2d9b3234eb0c1f306d46c4
hassan <alrashid334@gmail.com>
2019-07-25 10:44:15 +0300

    Table2 Test

--- app.js
+++ app.js
@@ -37,6 +37,46 @@ connection.query(queryString1,[itemNo], (err, rows, fields) => {
     })
     res.json(item)
 })
+
+//TEST FOR TABLE DETAILS:
+const itemNo = req.params.No
+const queryString2 = "SELECT * FROM tableDetails WHERE No = ?"
+connection.query(queryString2,[itemNo], (err, rows, fields) => {
+    if (err){
+        console.log("failed to query for items" +err)
+        res.sendStatus(500)
+        res.end()
+        return 
+
+    }
+    console.log("fetched items succesfully ")
+    const item = rows.map((row)=> {
+        return { No: row.No,
+                item: row.item,
+                Description: row.Description,
+                Impact_Remars_Comments: row.Impact_Remars_Comments,
+                Type: row.Type,
+                Division: row.Division,
+                Proponent: row.Proponent,
+                is_Proponent_involve: row.is_Proponent_involve,
+                Fund_Source: row.Fund_Source,
+                CAD_SME: row.CAD_SME,
+                Theme: row.Theme,
+                start: row.start,
+                end: row.end,
+                cost: row.cost,
+                NPV: row.NPV,
+                NPV_Justification: row.NPV_Justification,
+                DT_Item: row.DT_Item,
+                BP_Item: row.BP_Item,
+                Demand: row.Demand,
+                Note: row.Note,
+                Status: row.Status,
+                Update: row.Update
+                }
+    })
+    res.json(item)
+})
 //res.end()
 })
 
