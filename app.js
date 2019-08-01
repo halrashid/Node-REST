f7023459cd762f5a7b73f6f439052f9be419838e
shoruq <salufaisan@hotmail.com>
2019-08-01 12:31:40 +0300

    details based on track input

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mysql = require('mysql')
 
 app.use(morgan('combined'))
 
-app.get('/Items/:Page',(req,res)=> {
+app.get('/Items/:Page/:Type',(req,res)=> {
     console.log("fetching" +req.params.Page)
     
     const connection = mysql.createConnection({
@@ -17,8 +17,9 @@ app.get('/Items/:Page',(req,res)=> {
     
     })
     const page = req.params.Page
-    const queryString1 = "SELECT * FROM tableDetails"
-    connection.query(queryString1,[], (err, rows, fields) => {
+    const itemType = req.params.Type
+    const queryString1 = "SELECT * FROM tableDetails WHERE Type = ?"
+    connection.query(queryString1,[itemType], (err, rows, fields) => {
         if (err){
             console.log("failed to query for items" +err)
             res.sendStatus(500)
@@ -29,9 +30,9 @@ app.get('/Items/:Page',(req,res)=> {
         console.log("fetched items succesfully ")
         const item = rows.map((row)=> {
             return { No: row.No,
-                item: row.Item,
+                Item: row.Item,
                 Description: row.Description,
-                Impact_Remars_Comments: row.Impact_Remars_Comments,
+                Impact_Reamars_Comments: row.Impact_Reamars_Comments,
                 Type: row.Type,
                 Division: row.Division,
                 Proponent: row.Proponent,
@@ -54,115 +55,10 @@ app.get('/Items/:Page',(req,res)=> {
         })
         res.json(item)
     })
-    })
-
-app.get('/Item/:No',(req,res)=> {
-console.log("fetching" +req.params.No)
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user:'root',
-    password: '123456shoruq',
-    database:'m'
-
-})
-const itemNo = req.params.No
-const queryString1 = "SELECT * FROM table1 WHERE No = ?"
-connection.query(queryString1,[itemNo], (err, rows, fields) => {
-    if (err){
-        console.log("failed to query for items" +err)
-        res.sendStatus(500)
-        res.end()
-        return 
-
-    }
-    console.log("fetched items succesfully ")
-    const item = rows.map((row)=> {
-        return { 
-            No: row.No,
-            item: row.item,
-            Description: row.Description,
-            Impact_Remars_Comments: row.Impact_Remars_Comments,
-            Type: row.Type,
-            Division: row.Division,
-            Proponent: row.Proponent,
-            is_Proponent_involve: row.is_Proponent_involve,
-            Fund_Source: row.Fund_Source,
-            CAD_SME: row.CAD_SME,
-            Theme: row.Theme,
-            start: row.start,
-            end: row.end,
-            cost: row.cost,
-            NPV: row.NPV,
-            NPV_Justification: row.NPV_Justification,
-            DT_Item: row.DT_Item,
-            BP_Item: row.BP_Item,
-            Demand: row.Demand,
-            Note: row.Note,
-            Status: row.Status,
-            Update: row.Update
-        }
-    })[0]
-    res.json(item)
-})
-})
-
-
-app.get('/ItemDetails/:No',(req,res)=> {
-    console.log("fetching" +req.params.No)
-    
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user:'root',
-        password: '123456shoruq',
-        database:'m'
-    
-    })
-//TEST FOR TABLE DETAILS:
-const itemNo = req.params.No
-const queryString2 = "SELECT * FROM tableDetails WHERE No = ?"
-connection.query(queryString2,[itemNo], (err, rows, fields) => {
-    if (err){
-        console.log("failed to query for items" +err)
-        res.sendStatus(500)
-        res.end()
-        return 
-
-    }
-
-    console.log("fetched items succesfully ")
-    const item = rows.map((row)=> {
-        return { No: row.No,
-                item: row.item,
-                Description: row.Description,
-                Impact_Remars_Comments: row.Impact_Remars_Comments,
-                Type: row.Type,
-                Division: row.Division,
-                Proponent: row.Proponent,
-                is_Proponent_involve: row.is_Proponent_involve,
-                Fund_Source: row.Fund_Source,
-                CAD_SME: row.CAD_SME,
-                Theme: row.Theme,
-                start: row.start,
-                end: row.end,
-                cost: row.cost,
-                NPV: row.NPV,
-                NPV_Justification: row.NPV_Justification,
-                DT_Item: row.DT_Item,
-                BP_Item: row.BP_Item,
-                Demand: row.Demand,
-                Note: row.Note,
-                Status: row.Status,
-                Update: row.Update
-                }
-    })[0];
-    res.json(item)
-})
-//res.end()
 })
 
 app.get("/", (req, res)=>{
-    res.send("Hello he is a root! ")
+    res.send("Hello he is a groot! ")
 })
 
 app.get("/users", (req, res) => {
@@ -173,5 +69,3 @@ app.get("/users", (req, res) => {
 app.listen(3003, () => {
     console.log("Server is running")
 })
-
-
