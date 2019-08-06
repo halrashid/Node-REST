7cce02a393bbfa87b5c735082cca3c7f90a63c82
shoruq <salufaisan@hotmail.com>
2019-08-06 14:59:22 +0300

    post request

--- app.js
+++ app.js
@@ -2,30 +2,35 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mysql = require('mysql')
+const bodyParser = require('body-parser')
 
+app.use(morgan('short'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(morgan('combined'))
+function getConnection(){
+    return  mysql.createConnection({
+            host: 'localhost',
+            user:'root',
+            password: '123456shoruq',
+            database:'m'
+        });
+}
 
 app.get('/Items/:Page/:Type',(req,res)=> {
     console.log("fetching" +req.params.Page)
     
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user:'root',
-        password: '123456shoruq',
-        database:'m'
-    
-    })
-    const page = req.params.Page
-    const itemType = req.params.Type
-    const queryString1 = "SELECT * FROM tableDetails WHERE Type = ?"
+    const connection = getConnection();
+
+    const page = req.params.Page;
+    const itemType = req.params.Type;
+    const queryString1 = "SELECT * FROM tableDetails WHERE Type = ?";
     connection.query(queryString1,[itemType], (err, rows, fields) => {
         if (err){
             console.log("failed to query for items" +err)
             res.sendStatus(500)
             res.end()
             return 
-    
         }
         console.log("fetched items succesfully ")
         const item = rows.map((row)=> {
@@ -54,18 +59,78 @@ app.get('/Items/:Page/:Type',(req,res)=> {
             }
         })
         res.json(item)
+    });
+});
+
+app.post("/Item", (req, res) => {
+    const connection = getConnection();
+    const bodyJson = req.body;
+    console.log('printing body', bodyJson);
+
+    const columns = "("
+        + "Item, "
+        + "Description, "
+        + "Type, "
+        + "Impact_Reamars_Comments, "
+        + "Division, "
+        + "Proponent, "
+        + "is_Proponent_involve, "
+        + "Fund_Source, "
+        + "CAD_SME, "
+        + "Theme, "
+        + "start, "
+        + "end, "
+        + "cost, "
+        + "NPV, "
+        + "NPV_Justification, "
+        + "DT_Item, "
+        + "BP_Item, "
+        + "Demand, "
+        + "Note, "
+        + "Status, "
+        + "Last_Update"
+        + ")";
+
+    const values = [
+        bodyJson.Item,
+        bodyJson.Description,
+        bodyJson.Type,
+        bodyJson.Impact_Reamars_Comments,
+        bodyJson.Division,
+        bodyJson.Proponent,
+        bodyJson.is_Proponent_involve,
+        bodyJson.Fund_Source,
+        bodyJson.CAD_SME,
+        bodyJson.Theme,
+        bodyJson.start,
+        bodyJson.end,
+        bodyJson.cost,
+        bodyJson.NPV,
+        bodyJson.NPV_Justification,
+        bodyJson.DT_Item,
+        bodyJson.BP_Item,
+        bodyJson.Demand,
+        bodyJson.Note,
+        bodyJson.Status,
+        bodyJson.Last_Update
+    ];
+    const questions = "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    const insertQuery = " INSERT INTO tableDetails " + columns +" VALUES " + questions;
+    connection.query(insertQuery, values, (err, rows, fields) => {
+        if (err){
+            console.log("failed to query for insert item" + err);
+            res.sendStatus(500);
+            res.end();
+            return;
+        }
+        res.json(bodyJson);
     })
-})
+});
 
 app.get("/", (req, res)=>{
     res.send("Hello he is a groot! ")
-})
-
-app.get("/users", (req, res) => {
-    var user1 = {FN: "Hassan"}
-    res.json(user1)
-})
+});
 
 app.listen(3003, () => {
     console.log("Server is running")
-})
+});
